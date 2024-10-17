59d82408c28bb3a949523615c8cbab4d2fc7ab0e
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2024-10-18 01:07:44 +0530

    unused imports removed

--- app/src/main/java/com/example/passwordmanagerseconddraft/auth/AuthScreen.kt
+++ app/src/main/java/com/example/passwordmanagerseconddraft/auth/AuthScreen.kt
@@ -39,9 +39,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.passwordmanagerseconddraft.screens.AppHeading
 
 @Composable
 fun LoginPage(
