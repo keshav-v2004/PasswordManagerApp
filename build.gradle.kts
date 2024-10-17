b11c2a807a7a7503c1a6dbfe4dcde298b420dad2
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2024-10-17 16:14:18 +0530

    misc

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
